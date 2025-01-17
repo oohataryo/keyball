e540d91a8f2bc4293cf12b31341674d738acc621
oohataryo <62413461+oohataryo@users.noreply.github.com>
2025-01-17 17:26:46 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -54,24 +54,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
+  uint8_t layer = biton32(state);
+    switch (layer) {
+        case 0:
+            rgblight_sethsv(HSV_RED);
+            break;
+        case 1:
+            rgblight_sethsv(HSV_AZURE);
+            break;
+        case 2:
+            rgblight_sethsv(HSV_GREEN);
+            break;
+        case 4:
+            rgblight_sethsv(HSV_AZURE);
+            break;
+        case 3:
+            rgblight_sethsv(HSV_BLUE);
+            break;        
+        case 5:
+            rgblight_sethsv(HSV_AZURE);
+            break;
+    }
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
-  switch (get_highest_layer(state)) {
-    case click_layer:
-      rgblight_sethsv(HSV_WHITE);
-      break;
-    case 1:
-      rgblight_sethsv(HSV_RED);
-      break;
-    case 2:
-      rgblight_sethsv(HSV_YELLOW);
-      break;
-    case 3:
-      rgblight_sethsv(HSV_GREEN);
-      break;
-
-    default:
-      rgblight_sethsv(HSV_OFF);
     return state;
 }
 
