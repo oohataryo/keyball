0338a2b8e628ba7ef02524f33aa64b3abd7a09fc
oohataryo <62413461+oohataryo@users.noreply.github.com>
2025-01-17 19:55:45 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -54,9 +54,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  // Auto enable scroll mode when the highest layer is 3
-  keyball_set_scroll_mode(get_highest_layer(state) == 3);
+  // Auto enable scroll mode when the highest layer is 7
+  keyball_set_scroll_mode(get_highest_layer(state) == 7);
  
+  uint8_t layer = biton32(state);
+    switch (layer) {
+        case 0:
+            rgblight_sethsv(HSV_RED);
+            break;
+        case 1:
+            rgblight_sethsv(HSV_AZURE);
+            break;
+        case 2:
+            rgblight_sethsv(HSV_GREEN);
+            break;
+        case 3:
+            rgblight_sethsv(HSV_AZURE);
+            break;
+        case 4:
+            rgblight_sethsv(HSV_BLUE);
+            break;        
+        case 5:
+            rgblight_sethsv(HSV_AZURE);
+            break;
+        case 6:
+            rgblight_sethsv(HSV_ORANGE);
+            break;
+    }
     return state;
 }
 
