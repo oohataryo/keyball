fec98ee852b820f9124155d4e6bf33b861aed1b9
oohataryo <62413461+oohataryo@users.noreply.github.com>
2025-01-17 19:14:55 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -56,7 +56,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
   // Auto enable scroll mode when the highest layer is 3
   keyball_set_scroll_mode(get_highest_layer(state) == 3);
-  state = update_tri_layer_state(state, 1, 2, 3, 4, 5, 6);
+  state = update_tri_layer_state(state, 1, 2, 3);
+  state = update_tri_layer_state(state, 4, 5, 6);
   uint8_t layer = biton32(state);
     switch (layer) {
         case 0:
