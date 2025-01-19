30f34bd9d24ec8c485942f727f00f9d8427f1837
oohataryo <62413461+oohataryo@users.noreply.github.com>
2025-01-20 02:53:05 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -37,7 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 #define LAYER_STATE_8BIT
 #define OLED_TIMEOUT 30000 // ミリ秒
-#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 6
 #define NO_MUSIC_MODE
