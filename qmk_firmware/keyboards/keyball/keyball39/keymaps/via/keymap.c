93192cd6c8f9f3ee5ab547834827f20cf9d00d69
oohataryo <62413461+oohataryo@users.noreply.github.com>
2025-01-20 15:03:02 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -17,20 +17,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-#include "keymap_jp.h"
+// #include "keymap_jp.h"
 
 #include "quantum.h"
 
- enum combos {
-   ALTR_ALTSFT,
-   ALTF_ALTCTRL
- };
- const uint16_t PROGMEM altr_combo[] = {KC_LALT, KC_R, COMBO_END};
- const uint16_t PROGMEM altf_combo[] = {KC_LALT, KC_F, COMBO_END};
- combo_t key_combos[] = {
-   [ALTR_ALTSFT] = COMBO(altr_combo, LALT(S(KC_NO))),
-   [ALTF_ALTCTRL] = COMBO(altf_combo, LALT(LCTL(KC_NO))),
- };
+// enum combos {
+//   ALTR_ALTSFT,
+//   ALTF_ALTCTRL
+// };
+// const uint16_t PROGMEM altr_combo[] = {KC_LALT, KC_R, COMBO_END};
+// const uint16_t PROGMEM altf_combo[] = {KC_LALT, KC_F, COMBO_END};
+// combo_t key_combos[] = {
+//   [ALTR_ALTSFT] = COMBO(altr_combo, LALT(S(KC_NO))),
+//   [ALTF_ALTCTRL] = COMBO(altf_combo, LALT(LCTL(KC_NO))),
+// };
  enum {
      TD_H,
  };
